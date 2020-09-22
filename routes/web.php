d3a6b1448f5b0103ea64d4abda0051db1af6283f
Jual Ahmed <md.jual.ah@gmail.com>
2020-09-22 21:51:36 +0600

    df

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
