cec9d8f2ecaf15a8ea21c14d0690eee4f01715e8
Jual Ahmed <md.jual.ah@gmail.com>
2020-09-26 01:17:39 +0600

    sf

--- public/argon/js/crud.js
+++ public/argon/js/crud.js
@@ -1,35 +1,35 @@
 //******    Category store    ******** */ / 
 $('#category_store').on('submit', function(event){
-    event.preventDefault();
-    $.ajax({
-        url : '/category',
-        method:"POST",
-        data: $(this).serialize(),
-        dataType:'JSON',
-        success : function(response) {
-            if(response == 'success'){
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Product Create successfully!',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-                $('.bd-example-modal-lg').modal('hide');
-            }else{
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'error',
-                    title: 'An error across',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-            }
-        }
-    });
-  });
+		event.preventDefault();
+		$.ajax({
+				url : '/category',
+				method:"POST",
+				data: $(this).serialize(),
+				dataType:'JSON',
+				success : function(response) {
+						if(response == 'success'){
+								Swal.fire({
+										position: 'top-end',
+										icon: 'success',
+										title: 'Product Create successfully!',
+										showConfirmButton: false,
+										timer: 1500
+								})
+								$('.bd-example-modal-lg').modal('hide');
+						}else{
+								Swal.fire({
+										position: 'top-end',
+										icon: 'error',
+										title: 'An error across',
+										showConfirmButton: false,
+										timer: 1500
+								})
+						}
+				}
+		});
+	});
 
-  //******    Category Find Edit Data    ******** */ / 
+	//******    Category Find Edit Data    ******** */ / 
 function find_category(id)
 {
 	$.ajax({
@@ -45,35 +45,35 @@ function find_category(id)
 }
 
 
-  //******    Category Update    ******** */ / 
+	//******    Category Update    ******** */ / 
 
 $('#category_update').on('submit', function(event){
-    event.preventDefault();
-    var id = $('#id').val();
-      $.ajax({
-          url : "/category/"+id,
-          method:"POST",
-          data: $(this).serialize(),
-          dataType:'JSON',
-          success : function(response) {
-              if(response == 'success'){
-                  Swal.fire({
-                      position: 'top-end',
-                      icon: 'success',
-                      title: 'Product Update successfully!',
-                      showConfirmButton: false,
-                      timer: 1500
-                  })
-                  $('.bd-update-lg').modal('hide');
-              }else{
-                  Swal.fire({
-                      position: 'top-end',
-                      icon: 'error',
-                      title: 'An error across',
-                      showConfirmButton: false,
-                      timer: 1500
-                  })
-              }
-          }
-      });
-  });
+		event.preventDefault();
+		var id = $('#id').val();
+			$.ajax({
+					url : "/category/"+id,
+					method:"POST",
+					data: $(this).serialize(),
+					dataType:'JSON',
+					success : function(response) {
+							if(response == 'success'){
+									Swal.fire({
+											position: 'top-end',
+											icon: 'success',
+											title: 'Product Update successfully!',
+											showConfirmButton: false,
+											timer: 1500
+									})
+									$('.bd-update-lg').modal('hide');
+							}else{
+									Swal.fire({
+											position: 'top-end',
+											icon: 'error',
+											title: 'An error across',
+											showConfirmButton: false,
+											timer: 1500
+									})
+							}
+					}
+			});
+	});
