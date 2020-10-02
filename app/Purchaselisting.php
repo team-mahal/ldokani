37dc7b6420649b8d47db8b43837a6f928cca4515
mdjamilaj <mdjamilaj1@gmail.com>
2020-10-02 21:45:01 +0600

    listing

--- app/Purchaselisting.php
+++ app/Purchaselisting.php
@@ -9,4 +9,19 @@ class Purchaselisting extends Model
 {
     use SoftDeletes;
     protected $guarded = [];
+
+    public function distributor() 
+    {
+        return $this->belongsTo(Distributor::class, 'distributor_id'); 
+    }
+
+    public function purchasereceipt() 
+    {
+        return $this->belongsTo(Purchasereceipt::class, 'purchasereceipt_id'); 
+    }
+
+    public function product() 
+    {
+        return $this->belongsTo(Product::class, 'product_id'); 
+    }
 }
--- resources/views/setup/purchaselisting/index.blade.php
+++ resources/views/setup/purchaselisting/index.blade.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="{{ asset('argon') }}/css/dark.css" type="text/css">
 
 <div class="header bg-gradient-primary pt-5 pt-md-7"></div>
-<div class="container-fluid min-700px" id="vuejscom">
+<div class="container-fluid min-700px">
 
 	{{-- purchasereceipt List  --}}
 	<div class="col-xl-12">
@@ -54,12 +54,11 @@
 					<thead class="thead-light">
 						<tr>
 							<th scope="col">#</th>
-							<th scope="col">Distributor</th>
-							<th scope="col">purchasereceipt Amount</th>
-							<th scope="col">Transport Cost</th>
-							<th scope="col">Final Amount</th>
-							<th scope="col">Total Paid</th>
-							<th scope="col">Receipt Status</th>
+							<th scope="col">Pr. ID</th>
+							<th scope="col">Product Name</th>
+							<th scope="col">Quantity</th>
+							<th scope="col">Unit Buy Price</th>
+							<th scope="col">Total Buy Price</th>
 							<th scope="col">Action</th>
 						</tr>
 					</thead>
@@ -67,12 +66,11 @@
 						@foreach($datas as $key => $data)
 						<tr>
 							<td scope="row">{{ $key+1 }}</td>
-							<td style="">{{$data->distributor->name}}</td>
-							<td style="">{{$data->amount}}</td>
-							<td style="">{{$data->transport_cost}}</td>
-							<td style="">{{$data->final_amount}}</td>
-							<td style="">{{$data->total_paid}}</td>
-							<td style="">@if(empty($data->payment_amount)) {{ "Unpaid" }} @else {{ "Paid" }} @endif</td>
+							<td style="">{{$data->purchasereceipt_id}}</td>
+							<td style="">{{$data->product->name}}</td>
+							<td style="">{{$data->quantity}}</td>
+							<td style="">{{$data->unit_buy_price}}</td>
+							<td style="">{{$data->total_buy_price}}</td>
 							<td style="display: -webkit-inline-box;">
 								<button type="button" class="btn btn-sm btn-success" data-toggle="modal"
 									data-target=".bd-update-lg" onclick="find_purchasereceipt({{$data->id}})">Edit</button>
@@ -111,17 +109,17 @@
 					</button>
 				</div>
 				<div class="modal-body">
-					<div class="row">
+					<div class="row"  id="vuejscom">
 						<div class="col-lg-12 col-md-12 col-sm-12">
 							<div class="form-group">
-								<label for="distributor_id"> Distributor: * </label>
+								<label for="distributor_id"> Select Receipt * </label>
 								<multiselect  v-model="selectedpurchasereceipts" :options="purchasereceipts" hideSelected="true" allowEmpty="false" placeholder="Type To Search Distributor" track-by="name" label="name">
 									<template slot="option" slot-scope="props">
 										<div class="option__desc"><span class="option__title" v-html="props.option.distributor.name"></span></div>
 									</template>
 									<template slot="singleLabel" slot-scope="{ option }"><strong v-html="option.distributor.name"></strong> </template>
 								</multiselect>
-								<input type="text" name="distributor_id"  :value="selectedpurchasereceipts.id" required style="opacity: 0;height: 0px;width: 0px;transform: translate(20px, -30px);">
+								<input type="text" name="purchasereceipt_id"  :value="selectedpurchasereceipts.id" required style="opacity: 0;height: 0px;width: 0px;transform: translate(20px, -30px);">
 							</div>
 						</div>
 						<div class="col-12">
@@ -175,6 +173,8 @@
 								</div>
 							</div>
 						</div>
+					</div>
+					<div class="row">
 						<div class="col-lg-6 col-md-12 col-sm-12">
 							<div class="form-group">
 								<label> Quantity </label>
@@ -335,7 +335,6 @@
 <script type="application/javascript">
 
 setTimeout(function(){
-
 	Vue.component('multiselect', window.VueMultiselect.default)
 	const vm = new Vue({
 	  el:"#vuejscom",
@@ -347,48 +346,6 @@ setTimeout(function(){
 				selectedpurchasereceipts:[],
 
 			};
-		},
-		methods: {
-			store: function(event) {
-				event.preventDefault();
-				console.log(this);
-				// $.ajax({
-				// 	url     : '/get-product',
-				// 	cash    : false,
-				// 	method  :"POST",
-				// 	data: new FormData(this),
-				// 	dataType: 'json',
-				// 	success : function(re)
-				// 	{	
-				// 		self.products=re
-				// 	}
-				// })
-			},
-			stringtonumber(number){
-			  return number ? parseInt(number) : 0;
-			},
-			getFormValues() {
-			  var self = this;
-				$.ajax({
-					url     : base_url+'admin/questionPaperSetup/store',
-					data :{
-					selectedproduct : self.selectedproduct,
-					selectedquestion : self.array
-					},
-					cash    : false,
-					dataType: 'json',
-					type: "POST",
-					success : function(re)
-					{	
-					if(re == 1){
-						sessionStorage.setItem("sms", "Create Succefully");
-						window.location.replace(base_url+'admin/questionPaperSetup/index');
-					}else{
-						alert("Opps!! An Error");
-					}
-					}
-				});
-			}
 		},
 		created(){
 			var self = this;
